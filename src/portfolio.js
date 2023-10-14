93ec343a9eb7cf096a5d0c1fd39de34f8dd7d6f3
Hemanth-Thaluru <thaluruhemanth105@gmail.com>
2023-10-13 22:15:56 -0400

    updated portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -6,7 +6,7 @@ import splashAnimation from "./assets/lottie/splashAnimation";
 const splashScreen = {
   enabled: true, // set false to disable splash screen
   animation: splashAnimation,
-  duration: 2000 // Set animation duration as per your animation
+  duration: 2000 
 };
 
 // Summary And Greeting Section
@@ -22,7 +22,7 @@ const greeting = {
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web with JavaScript / Angular /Reactjs / Nodejs / .NET and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1UyGLN8F1MJdUKolTs66Nv0lWOS_a6vLG/view?usp=drive_link",
+    "https://drive.google.com/file/d/1MRnkgNpFhFkXO1t5ycoWDUNG3rBLhgAY/view?usp=drive_link",
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
@@ -46,11 +46,10 @@ const skillsSection = {
   subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
   skills: [
     emoji(
-      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+      "⚡ Develop highly interactive Front end / User Interfaces along with Back end and Devops for your web applications"
     ),
-    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
     emoji(
-      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+      "⚡ Integration of third party services such as Azure/ AWS / fire base"
     )
   ],
 
@@ -59,15 +58,11 @@ https://fontawesome.com/icons?d=gallery */
 
   softwareSkills: [
     {
-      skillName: "html-5",
-      fontAwesomeClassname: "fab fa-html5"
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
     },
     {
-      skillName: "css3",
-      fontAwesomeClassname: "fab fa-css3-alt"
-    },
-    {
-      skillName: "sass",
+      skillName: "angular",
       fontAwesomeClassname: "fab fa-angular"
     },
     {
@@ -75,16 +70,8 @@ https://fontawesome.com/icons?d=gallery */
       fontAwesomeClassname: "fab fa-js"
     },
     {
-      skillName: "reactjs",
-      fontAwesomeClassname: "fab fa-react"
-    },
-    {
       skillName: "nodejs",
       fontAwesomeClassname: "fab fa-node"
-    },
-    {
-      skillName: "swift",
-      fontAwesomeClassname: "fab fa-swift"
     },
     {
       skillName: "npm",
@@ -94,10 +81,7 @@ https://fontawesome.com/icons?d=gallery */
       skillName: "sql-database",
       fontAwesomeClassname: "fas fa-database"
     },
-    {
-      skillName: "aws",
-      fontAwesomeClassname: "fab fa-aws"
-    },
+
     {
       skillName: "firebase",
       fontAwesomeClassname: "fas fa-fire"
@@ -109,7 +93,19 @@ https://fontawesome.com/icons?d=gallery */
     {
       skillName: "docker",
       fontAwesomeClassname: "fab fa-docker"
-    }
+    },
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "git",
+      fontAwesomeClassname: "fab fa-git"
+    },
   ],
   display: true // Set false to hide this section, defaults to true
 };
@@ -182,7 +178,11 @@ const workExperiences = {
       company: "Cognizant",
       companylogo: require("./assets/images/cognizant.png"),
       date: "March 2021 – April 2022",
-      desc: "Developed RESTful APIs using ASP.NET Web API to improve system communication and enhance data accessibility"
+      desc: "Developed RESTful APIs using ASP.NET Web API to improve system communication and enhance data accessibility",
+      descBullets: [
+        "Collabrated with a team for high-performance product E2E from identifying system requirements to software implementation",
+        "Deployed multi staged CI/CD pipeline with version locking and roll-back capabilities, performed code review, curated documentation"
+      ]
     }
   ]
 };
@@ -192,35 +192,45 @@ To know how to get github key look at readme.md */
 
 const openSource = {
   showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Some big projects you have worked on
 
 const bigProjects = {
-  title: "Big Projects",
-  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  title: "Projects",
+  subtitle: "",
   projects: [
     {
-      image: require("./assets/images/saayaHealthLogo.webp"),
-      projectName: "Saayahealth",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("./assets/images/ub_bot.png"),
+      projectName: "UB Bot",
+      projectDesc: "Leveraged cutting-edge LangChain technology to perform comprehensive web scraping, collected data from various web pages of UB for a robust similarity search",
       footerLink: [
         {
-          name: "Visit Website",
-          url: "http://saayahealth.com/"
+          name: "Visit",
+          url: "https://github.com/Hemanth-Thaluru/LLM_UB/tree/main/ice_breaker"
         }
-        //  you can add extra buttons here.
       ]
     },
     {
-      image: require("./assets/images/nextuLogo.webp"),
-      projectName: "Nextu",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("./assets/images/snake_relearn.jpeg"),
+      projectName: "Snake ReLearn",
+      projectDesc: "Changed the classic Snake game using cutting-edge deep reinforcement learning techniques",
       footerLink: [
         {
-          name: "Visit Website",
-          url: "http://nextu.se/"
+          name: "Visit",
+          url: "https://github.com/Hemanth-Thaluru/reinforcement_learning"
+        }
+      ]
+    },
+    {
+      image: require("./assets/images/colorizer.jpeg"),
+      projectName: "Colorizer",
+      projectDesc: "Incorporates deep learning techniques to understand and predict reliable colors for different elements in an image",
+      footerLink: [
+        {
+          name: "Visit",
+          url: "https://huggingface.co/spaces/hemanth-thaluru/sdm-image-colorization-prj"
         }
       ]
     }
@@ -238,53 +248,68 @@ const achievementSection = {
 
   achievementsCards: [
     {
-      title: "Google Code-In Finalist",
+      title: "Microsoft Certified Azure Developer Associate",
       subtitle:
-        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
-      image: require("./assets/images/codeInLogo.webp"),
-      imageAlt: "Google Code-In Logo",
+        "Earned proficiency in designing and building scalable applications using Azure services, mastering SDKs and APIs.",
+      image: require("./assets/images/az-201.png"),
+      imageAlt: "Azure Developer Associate",
       footerLink: [
         {
-          name: "Certification",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
+          name: "Badge",
+          url: "https://www.credly.com/badges/6bb9af1d-6abe-4ce3-b62d-e9883e9df07e/public_url"
         },
         {
-          name: "Award Letter",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
+          name: "Certificate",
+          url: "https://drive.google.com/file/d/1GRfh0SmGPd8bj3tLWA1khG0xThjd9xv8/view?usp=drive_link"
         },
         {
-          name: "Google Code-in Blog",
-          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
+          name: "Passed Exam",
+          url: "https://learn.microsoft.com/en-us/credentials/certifications/exams/az-204/"
         }
       ]
     },
     {
-      title: "Google Assistant Action",
+      title: "Microsoft Certified Azure Data Scientist Associate",
       subtitle:
-        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
-      image: require("./assets/images/googleAssistantLogo.webp"),
-      imageAlt: "Google Assistant Action Logo",
+        "Proficiently applied machine learning techniques, statistical models, and data analytics to derive valuable insights.",
+      image: require("./assets/images/dp_100.png"),
+      imageAlt: "Azure Data Scientist Associate",
       footerLink: [
         {
-          name: "View Google Assistant Action",
-          url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
+          name: "Badge",
+          url: "https://www.credly.com/badges/87fb5d97-049e-4e3e-8ed3-84059159ef46/public_url"
+        },
+        {
+          name: "Certificate",
+          url: "https://drive.google.com/file/d/1mMvkwArTbFbAa8VAz3SVUIDLC4Kai3JE/view?usp=drive_link"
+        },
+        {
+          name: "Passed Exam",
+          url: "https://learn.microsoft.com/en-us/credentials/certifications/exams/dp-100/"
         }
       ]
     },
-
     {
-      title: "PWA Web App Developer",
-      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
-      image: require("./assets/images/pwaLogo.webp"),
-      imageAlt: "PWA Logo",
+      title: "Microsoft Certified PowerBI Data Analyst",
+      subtitle:
+        "Mastered data analysis and transformation using Power Query and DAX, contributing to informed decision-making.",
+      image: require("./assets/images/dp_100.png"),
+      imageAlt: "PowerBI Data Analyst",
       footerLink: [
-        {name: "Certification", url: ""},
         {
-          name: "Final Project",
-          url: "https://pakistan-olx-1.firebaseapp.com/"
+          name: "Badge",
+          url: "https://www.credly.com/badges/0ef0a840-c5c0-45c0-b953-80dbd9577d03/public_url"
+        },
+        {
+          name: "Certificate",
+          url: "https://drive.google.com/file/d/19ERigVsx9RcZJZqqItCb-UHHLthblGXL/view?usp=drive_link"
+        },
+        {
+          name: "Passed Exam",
+          url: "https://learn.microsoft.com/en-us/credentials/certifications/power-bi-data-analyst-associate/"
         }
       ]
-    }
+    },
   ],
   display: true // Set false to hide this section, defaults to true
 };
@@ -298,17 +323,11 @@ const blogSection = {
   displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
   blogs: [
     {
-      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      url: "https://medium.com/@canhemanth/the-art-of-feature-engineering-sculpting-data-for-prediction-a5c1ffe6b5a1",
+      title: "The Art of Feature Engineering: Sculpting Data for Prediction",
       description:
-        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+        "Why Feature Engineering is not just a mundane step in machine learning, but a captivating art that adds depth and insight to raw data..."
     },
-    {
-      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-      title: "Why REACT is The Best?",
-      description:
-        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
-    }
   ],
   display: true // Set false to hide this section, defaults to true
 };
